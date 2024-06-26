6ddd2d002eb1d7cc5fb8b8ae4ce23d9c9b8420fe
Sm4rtSt1ck <tranzistor404@gmail.com>
2024-06-27 01:39:17 +0300

    Add title, icon

--- src/main/java/ru/pasnin/vetclinic/Main.java
+++ src/main/java/ru/pasnin/vetclinic/Main.java
@@ -3,16 +3,19 @@ package ru.pasnin.vetclinic;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+
+        stage.setTitle("Vet system");
+        stage.getIcons().add(new Image("file:icon.png"));
         stage.setScene(scene);
         stage.show();
     }
